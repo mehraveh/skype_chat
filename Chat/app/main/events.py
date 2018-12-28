a1d5e1af9e83d4a7209958dec6a093c10df0dfd3
mehraveh ahmadi <mehraveh.ahmadi@yahoo.com>
2018-12-28 18:42:04 +0330

    Add saving PMs and Add styles to chat UI

--- Chat/app/main/events.py
+++ Chat/app/main/events.py
@@ -1,6 +1,13 @@
+import random, string
+
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
+from . models import *
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
 
 
 @socketio.on('joined', namespace='/chat')
@@ -13,6 +20,10 @@ def joined(message):
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
+    length = random.randint(10,25)
+    pm_id  = randomword(length)
+    pm = SkypePmModel(pm_id=pm_id, sender=session.get('name'), text= message['msg'])
+    pm.save()
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
 
 
