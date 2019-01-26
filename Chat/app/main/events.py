fc9e560f842b118e153e0f9a745cf8760d22bbeb
mehraveh ahmadi <mehraveh.ahmadi@yahoo.com>
2019-01-26 12:58:46 +0330

    fix rooms bug

--- Chat/app/main/events.py
+++ Chat/app/main/events.py
@@ -27,11 +27,3 @@ def text(message):
     emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
 
 
-# @socketio.on('left', namespace='/chat')
-# def left(message):
-#     """Sent by clients when they leave a room.
-#     A status message is broadcast to all people in the room."""
-#     room = session.get('room')
-#     leave_room(room)
-#     emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
-
